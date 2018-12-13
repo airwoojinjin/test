5c166ae174f8a192c29db3d225fecc3485e50d22
airwoojinjin <airwoojinjin@gmail.com>
2018-12-13 14:10:09 +0900

    add lists

--- functional_test.py
+++ functional_test.py
@@ -1,3 +1,19 @@
-def application(env, start_response):
-	start_response('200 OK', [('Content-Type','text/html')])
-	return [b"Hello World"]
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://172.16.87.102')
+
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
