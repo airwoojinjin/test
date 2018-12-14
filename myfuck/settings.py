ecb3fe45c7f359fbb60440d9bfc05bf2b2f18ab0
airwoojinjin <airwoojinjin@gmail.com>
2018-12-14 13:32:37 +0900

    db

--- myfuck/settings.py
+++ myfuck/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myfuck.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fuck',
+        'USER': 'fuck',
+        'PASSWORD': 'admin!0920',
+        'HOST': 'test-postgres.cdoktyhs3h48.ap-northeast-2.rds.amazonaws.com',
+        'PORT': 5432
     }
 }
 
