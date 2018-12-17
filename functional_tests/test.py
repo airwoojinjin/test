f854f67ebebd90a3ef0caaed3fd8ddd43db747da
airwoojinjin <airwoojinjin@gmail.com>
2018-12-17 10:00:39 +0900

    1217

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+from django.test import LiveServerTestCase
+import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Chrome()
 
@@ -16,14 +18,14 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('1: Buy peacock feathers')
+		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
